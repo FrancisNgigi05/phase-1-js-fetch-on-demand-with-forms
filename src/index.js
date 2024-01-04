1ca93f071fb866c96b4df3a071b81eec2118b9d1
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-01-04 21:03:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const init = () => {
+const init = function() {
     const inputForm = document.querySelector('form');
-    inputForm.addEventListener('submit', event => {
+    inputForm.addEventListener('submit', function(event) {
         event.preventDefault();
         const input = document.querySelector('input#searchByID')
         // console.log(input.value);
 
         fetch(`http://localhost:3000/movies/${input.value}`)
-            .then(response => response.json())
-            .then(data=> {
+            .then(function(response) {return response.json()})
+            .then(function(data) {
                 const title = document.querySelector('section#movieDetails h4');
                 const summary = document.querySelector('section#movieDetails p');
 
